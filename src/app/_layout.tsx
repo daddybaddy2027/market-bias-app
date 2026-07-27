a81f8e2173304a806d01a1ad74f153cf9cae3525
daddybaddy2027 <milenkovicv01@gmail.com>
2026-07-27 18:51:38 +0200

    Register outlook route explicitly

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -21,18 +21,15 @@ export default function RootLayout() {
         >
           <Stack.Screen name="index" />
           <Stack.Screen name="macro" />
+          <Stack.Screen name="outlook" />
           <Stack.Screen name="login" />
           <Stack.Screen name="account" />
           <Stack.Screen name="pricing" />
           <Stack.Screen name="asset/[symbol]" />
         </Stack>
 
-        {Platform.OS === "web" ? (
-          <Analytics />
-        ) : null}
+        {Platform.OS === "web" ? <Analytics /> : null}
       </>
     </AuthProvider>
   );
 }
-
-
